83650ec9214cb559080ac4dc5d06146663eb9bb8
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-01-15 08:05:22 -0300

    Instalando React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './global.css'
 
-import { Button } from '@/components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Hello world</h1>
-      <Button variant="outline">Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
