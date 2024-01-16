88e63a2e1d478ffdd9aeb86e7119da156cf9be17
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-01-16 07:44:47 -0300

    Notificações toast (sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -24,6 +25,12 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     await new Promise<void>((resolve) => setTimeout(resolve, 2000))
     console.log(data)
+    toast.success('Enviamos um link de autenticação para seu e-mail.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
   }
   return (
     <>
