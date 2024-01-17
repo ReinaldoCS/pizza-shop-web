ddbebf854586948c6c318127da842681cec8e5a4
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-01-17 07:51:57 -0300

    ThemeToggle e menu da conta

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,45 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="flex select-none items-center gap-2"
+          variant="outline"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56" align="end">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Reinaldo Correia</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            contato@reinaldodev.com.br
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer text-red-500 dark:text-red-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
