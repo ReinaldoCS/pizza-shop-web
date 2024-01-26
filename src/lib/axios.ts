5feb522591f72fddfa5d0f1c122d6e7e076a0e4d
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-01-26 07:52:30 -0300

    Loading das métricas

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,7 +9,9 @@ export const api = axios.create({
 
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000)) // 2s
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 3000)),
+    ) // Delay aleatório dentre 1 a 3 segundos por requisição
 
     return config
   })
