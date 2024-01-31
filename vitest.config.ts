acf1f2c8552ab4e28bd84e9624bd8e4d8906137e
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-01-31 07:57:28 -0300

    Teste unitário: Pagination

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 // vitest.config.ts
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
