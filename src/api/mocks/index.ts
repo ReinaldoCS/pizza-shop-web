5f53df08a4405161d6ed37ee243ab501f22ba891
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-02-02 07:06:46 -0300

    Setup do MSW

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,14 @@
+// import { http, HttpResponse } from 'msw'
+import { setupWorker } from 'msw/browser'
+
+import { env } from '@/env'
+
+export const worker = setupWorker()
+
+export async function enableMSW() {
+  if (env.MODE !== 'mock') {
+    return
+  }
+
+  await worker.start()
+}
